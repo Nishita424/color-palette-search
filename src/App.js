f8cb41b96e20c19eae5e8271ccc468af4449c639
nishitagiduturi <nishitaoct26@gmail.com>
2021-09-18 23:21:16 +0530

    1. Get set of colors matching the given search term from a static set of colors 2. Using styled components

--- src/App.js
+++ src/App.js
@@ -1,25 +1,128 @@
-import logo from './logo.svg';
-import './App.css';
+import { React, useState } from "react";
+import styled from "styled-components";
+
+const Container = styled.div`
+	display: flex;
+	flex-direction: column;
+	align-items: center;
+	justify-content: space-between;
+	margin: 4rem;
+`;
+
+const Input = styled.div`
+	display: flex;
+	align-items: center;
+	gap: 10px;
+	label {
+		color: VIOLET;
+	}
+	input {
+		margin: auto;
+		padding: 10px;
+		border: 2px solid DEEPSKYBLUE;
+		border-radius: 10px;
+		color: red;
+	}
+`;
+
+const ColorPalette = styled.div`
+	display: grid;
+	margin-top: 4rem;
+	grid-gap: 10px;
+	grid-template-columns: 100px 100px 100px 100px;
+	${
+		"" /* ${(searchResults) => (searchResults ? `display: grid;` : `display: block;`)} */
+	}
+`;
+
+const ColorBlock = styled.div`
+	border: 2px solid yellow;
+	border-radius: 5px;
+	height: 100px;
+	width: 100px;
+	${({ color }) => `background: ${color};`}
+`;
+
+const NoResults = styled.h2`
+	background: yellow;
+	width: 280px;
+	text-align: center;
+	margin: 0 5rem;
+`;
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+	const defaultColors = [
+		"skyblue",
+		"deeppink",
+		"hotpink",
+		"palevioletred",
+		"lightgray",
+		"gray",
+		"green",
+		"lightgreen",
+		"deepskyblue",
+		"aliceblue",
+		"antiquewhite",
+		"darkblue",
+		"darkviolet",
+		"indianred",
+		"lavender",
+		"coral",
+		"darkgray",
+		"gold",
+		"blue",
+		"dimgray",
+		"dodgerblue",
+		"darkslateblue",
+		"darkgreen",
+		"darksalmon",
+		"rosybrown",
+		"olivedrab",
+		"olive",
+	];
+	// const defaultColors = getRandomColors();
+
+	const [colorPalette, setColorPalette] = useState(defaultColors);
+	const [searchValue, setSearchValue] = useState("");
+
+	const onChangeSearchColor = (e) => {
+		const searchColor = e.target.value;
+		setSearchValue(searchColor);
+		if (searchColor) {
+			const matchingColors = defaultColors.filter((color) =>
+				color.includes(searchColor)
+			);
+			setColorPalette(matchingColors);
+		} else {
+			setColorPalette(defaultColors);
+		}
+	};
+
+	return (
+		<Container>
+			<Input>
+				<label>Color:</label>
+				<input
+					name="search"
+					value={searchValue}
+					type="text"
+					onChange={onChangeSearchColor}
+					placeholder="Type a color"
+				/>
+			</Input>
+			<ColorPalette>
+				{colorPalette.length ? (
+					colorPalette.map((color, index) => {
+						return (
+							<ColorBlock key={index} color={color}></ColorBlock>
+						);
+					})
+				) : (
+					<NoResults>No search results found</NoResults>
+				)}
+			</ColorPalette>
+		</Container>
+	);
 }
 
 export default App;
